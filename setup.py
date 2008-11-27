d13e1895eb96cedc04b8d88ba24db24e5a0a50bd
moe <moe>
2008-11-27 07:42:21 +0000

    merge pyflakes-setup.py-2709.  Fixes Pyflakes' setup.py.  author: moe, reviewer: mithrandi.  closes #2709

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author="Phil Frost",
     maintainer="Moe Aboulkheir",
     maintainer_email="moe@divmod.com",
-    url="http://www.divmod.org/projects/pyflakes",
-    packages=["pyflakes"],
+    url="http://www.divmod.org/trac/wiki/DivmodPyflakes",
+    packages=["pyflakes", "pyflakes.scripts", "pyflakes.test"],
     scripts=["bin/pyflakes"],
     long_description="""Pyflakes is program to analyze Python programs and detect various errors. It
 works by parsing the source file, not importing it, so it is safe to use on
