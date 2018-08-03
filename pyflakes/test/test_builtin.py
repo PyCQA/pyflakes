128c845152e75695d1b2f099bed4ac7a804e8577
John Vandenberg <jayvdb@gmail.com>
2018-08-03 22:35:06 +0700

    Allow some builtins to be deleted
    __builtins__, __file__ and __debug__ can be deleted.
    
    Adds a test runner that ensures defined builtins can be
    loaded, and can be deleted when the python runtime allows it.

--- pyflakes/test/test_builtin.py
+++ pyflakes/test/test_builtin.py
@@ -4,7 +4,14 @@ Tests for detecting redefinition of builtins.
 from sys import version_info
 
 from pyflakes import messages as m
+from pyflakes.checker import Checker
 from pyflakes.test.harness import TestCase, skipIf
+
+try:
+    WindowsError
+    WIN = True
+except NameError:
+    WIN = False
 
 
 class TestBuiltins(TestCase):
@@ -39,3 +46,51 @@ class TestBuiltins(TestCase):
 
         f()
         ''', m.UndefinedLocal)
+
+
+class TestLiveBuiltins(TestCase):
+
+    def test_exists(self):
+        for name in sorted(Checker.builtIns):
+            # __file__ does exist in this test harness
+            if name == '__file__':
+                continue
+
+            if name == 'WindowsError' and not WIN:
+                continue
+
+            source = '''
+            %s
+            ''' % name
+            e = self.pythonException(source)
+            self.assertIsNone(e)
+
+    def test_del(self):
+        for name in sorted(Checker.builtIns):
+            # __file__ does exist in this test harness
+            if name == '__file__':
+                continue
+
+            # __debug__ can be deleted sometimes and not deleted other times.
+            # Safest course of action is to assume it can be deleted, in
+            # order that no error is reported by pyflakes
+            if name == '__debug__':
+                continue
+
+            source = '''
+            del %s
+            ''' % name
+
+            e = self.pythonException(source)
+
+            if isinstance(e, SyntaxError):
+                if version_info < (3,):
+                    # SyntaxError: invalid syntax
+                    self.assertIn(name, ('print'))
+                else:
+                    # SyntaxError: can't delete keyword
+                    self.assertIn(name, ('None', 'True', 'False'))
+            elif isinstance(e, NameError):
+                self.flakes(source, m.UndefinedName)
+            else:
+                self.flakes(source)
