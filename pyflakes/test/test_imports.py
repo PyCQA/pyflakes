PyCQA/pyflakes

commit 47a164136982ea6ef80f302200b2a29d3a4b1c3c

checker.py: Add support for list concatenation
This adds support for when '__all__'
is defined from concatenated lists.
However, because pyflakes is not an
interpreter, this only applies to
certain reduced test cases.

Fixes https://github.com/PyCQA/pyflakes/issues/276

pyflakes/checker.py
@@ -390,10 +390,10 @@ class ExportBinding(Binding):
     can be determined statically, they will be treated as names for export and
     additional checking applied to them.
 
-    The only C{__all__} assignment that can be recognized is one which takes
-    the value of a literal list containing literal strings.  For example::
+    The only recognized C{__all__} assignment via list concatenation is in the
+    following format:
 
-        __all__ = ["foo", "bar"]
+        __all__ = ['a'] + ['b'] + ['c']
 
     Names which are imported and not otherwise used but appear in the value of
     C{__all__} will not have an unused import warning reported for them.
@@ -405,9 +405,25 @@ class ExportBinding(Binding):
         else:
             self.names = []
         if isinstance(source.value, (ast.List, ast.Tuple)):
-            for node in source.value.elts:
-                if isinstance(node, ast.Str):
-                    self.names.append(node.s)
+            self.names += ast.literal_eval(source.value)
+        # If concatenating lists
+        elif isinstance(source.value, ast.BinOp):
+            currentValue = source.value
+            while isinstance(currentValue.right, ast.List):
+                left = currentValue.left
+                right = currentValue.right
+                self.names += ast.literal_eval(right)
+                # If more lists are being added
+                if isinstance(left, ast.BinOp):
+                    currentValue = left
+                # If just two lists are being added
+                elif isinstance(left, ast.List):
+                    self.names += ast.literal_eval(left)
+                    # All lists accounted for - done
+                    break
+                # If not list concatenation
+                else:
+                    break
         super(ExportBinding, self).__init__(name, source)
 
 

pyflakes/test/test_imports.py
@@ -1068,19 +1068,14 @@ class TestSpecialAll(TestCase):
             __all__ += ['c', 'd']
         ''', m.UndefinedExport, m.UndefinedExport)
 
-    def test_unrecognizable(self):
+    def test_concatenationAssignment(self):
         """
-        If C{__all__} is defined in a way that can't be recognized statically,
-        it is ignored.
+        The C{__all__} variable is defined through list concatenation.
         """
         self.flakes('''
-        import foo
-        __all__ = ["f" + "oo"]
-        ''', m.UnusedImport)
-        self.flakes('''
-        import foo
-        __all__ = [] + ["foo"]
-        ''', m.UnusedImport)
+        import sys
+        __all__ = ['a'] + ['b'] + ['c']
+        ''', m.UndefinedExport, m.UndefinedExport, m.UndefinedExport, m.UnusedImport)
 
     def test_unboundExported(self):
         """

commit 08973344a45b1306d086f00e8cb5f5eb7d583378

Fix regression with `__all__ = [not, strings]`
Regression introduced in 47a164136982ea6ef80f302200b2a29d3a4b1c3c

Example trace before fix:

```
...

  File "/home/asottile/workspace/pyflakes/pyflakes/checker.py", line 998, in handleNodeStore
    binding = ExportBinding(name, node.parent, self.scope)
  File "/home/asottile/workspace/pyflakes/pyflakes/checker.py", line 426, in __init__
    self.names += ast.literal_eval(source.value)
  File "/usr/lib/python3.6/ast.py", line 85, in literal_eval
    return _convert(node_or_string)
  File "/usr/lib/python3.6/ast.py", line 61, in _convert
    return list(map(_convert, node.elts))
  File "/usr/lib/python3.6/ast.py", line 84, in _convert
    raise ValueError('malformed node or string: ' + repr(node))
ValueError: malformed node or string: <_ast.Attribute object at 0x7f6d2bf37b70>
```

pyflakes/checker.py
@@ -422,21 +422,27 @@ class ExportBinding(Binding):
             self.names = list(scope['__all__'].names)
         else:
             self.names = []
+
+        def _add_to_names(container):
+            for node in container.elts:
+                if isinstance(node, ast.Str):
+                    self.names.append(node.s)
+
         if isinstance(source.value, (ast.List, ast.Tuple)):
-            self.names += ast.literal_eval(source.value)
+            _add_to_names(source.value)
         # If concatenating lists
         elif isinstance(source.value, ast.BinOp):
             currentValue = source.value
             while isinstance(currentValue.right, ast.List):
                 left = currentValue.left
                 right = currentValue.right
-                self.names += ast.literal_eval(right)
+                _add_to_names(right)
                 # If more lists are being added
                 if isinstance(left, ast.BinOp):
                     currentValue = left
                 # If just two lists are being added
                 elif isinstance(left, ast.List):
-                    self.names += ast.literal_eval(left)
+                    _add_to_names(left)
                     # All lists accounted for - done
                     break
                 # If not list concatenation

pyflakes/test/test_imports.py
@@ -1093,6 +1093,33 @@ class TestSpecialAll(TestCase):
         __all__ = ['a'] + ['b'] + ['c']
         ''', m.UndefinedExport, m.UndefinedExport, m.UndefinedExport, m.UnusedImport)
 
+    def test_all_with_attributes(self):
+        self.flakes('''
+        from foo import bar
+        __all__ = [bar.__name__]
+        ''')
+
+    def test_all_with_names(self):
+        # not actually valid, but shouldn't produce a crash
+        self.flakes('''
+        from foo import bar
+        __all__ = [bar]
+        ''')
+
+    def test_all_with_attributes_added(self):
+        self.flakes('''
+        from foo import bar
+        from bar import baz
+        __all__ = [bar.__name__] + [baz.__name__]
+        ''')
+
+    def test_all_mixed_attributes_and_strings(self):
+        self.flakes('''
+        from foo import bar
+        from foo import baz
+        __all__ = ['bar', baz.__name__]
+        ''')
+
     def test_unboundExported(self):
         """
         If C{__all__} includes a name which is not bound, a warning is emitted.