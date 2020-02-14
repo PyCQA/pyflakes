04e96c8c40b7ec17aaaa8ab1d756f26ec0d62b30
Peter Law <PeterJCLaw@gmail.com>
2020-02-14 22:59:07 +0000

    Skip async function test where it doesn't apply

--- pyflakes/test/test_code_segment.py
+++ pyflakes/test/test_code_segment.py
@@ -1,7 +1,9 @@
+from sys import version_info
+
 from pyflakes import messages as m
 from pyflakes.checker import (FunctionScope, ClassScope, ModuleScope,
                               Argument, FunctionDefinition, Assignment)
-from pyflakes.test.harness import TestCase
+from pyflakes.test.harness import TestCase, skipIf
 
 
 class TestCodeSegments(TestCase):
@@ -125,6 +127,7 @@ class TestCodeSegments(TestCase):
         self.assertIsInstance(function_scope_bar['h'], Argument)
         self.assertIsInstance(function_scope_bar['i'], Argument)
 
+    @skipIf(version_info < (3, 5), 'new in Python 3.5')
     def test_scope_async_function(self):
         checker = self.flakes('''
         async def foo(a, b=1, *d, **e):
