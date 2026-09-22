4277a9a40b8c1dce4ca09ca2ab11a12c2ec807ef
anthony sottile <asottile@umich.edu>
2026-09-21 21:43:54 -0400

    replace confusing __str__ with imported_name (#890)

--- pyflakes/test/test_imports.py
+++ pyflakes/test/test_imports.py
@@ -16,83 +16,83 @@ class TestImportationObject(TestCase):
     def test_import_basic(self):
         binding = Importation('a', None, 'a', is_lazy=False)
         assert binding.source_statement == 'import a'
-        assert str(binding) == 'a'
+        assert binding.imported_name == 'a'
 
     def test_import_as(self):
         binding = Importation('c', None, 'a', is_lazy=False)
         assert binding.source_statement == 'import a as c'
-        assert str(binding) == 'a as c'
+        assert binding.imported_name == 'a as c'
 
     def test_import_submodule(self):
         binding = SubmoduleImportation('a.b', None, is_lazy=False)
         assert binding.source_statement == 'import a.b'
-        assert str(binding) == 'a.b'
+        assert binding.imported_name == 'a.b'
 
     def test_import_submodule_as(self):
         # A submodule import with an as clause is not a SubmoduleImportation
         binding = Importation('c', None, 'a.b', is_lazy=False)
         assert binding.source_statement == 'import a.b as c'
-        assert str(binding) == 'a.b as c'
+        assert binding.imported_name == 'a.b as c'
 
     def test_import_submodule_as_source_name(self):
         binding = Importation('a', None, 'a.b', is_lazy=False)
         assert binding.source_statement == 'import a.b as a'
-        assert str(binding) == 'a.b as a'
+        assert binding.imported_name == 'a.b as a'
 
     def test_importfrom_relative(self):
         binding = ImportationFrom('a', None, '.', 'a', is_lazy=False)
         assert binding.source_statement == 'from . import a'
-        assert str(binding) == '.a'
+        assert binding.imported_name == '.a'
 
     def test_importfrom_relative_parent(self):
         binding = ImportationFrom('a', None, '..', 'a', is_lazy=False)
         assert binding.source_statement == 'from .. import a'
-        assert str(binding) == '..a'
+        assert binding.imported_name == '..a'
 
     def test_importfrom_relative_with_module(self):
         binding = ImportationFrom('b', None, '..a', 'b', is_lazy=False)
         assert binding.source_statement == 'from ..a import b'
-        assert str(binding) == '..a.b'
+        assert binding.imported_name == '..a.b'
 
     def test_importfrom_relative_with_module_as(self):
         binding = ImportationFrom('c', None, '..a', 'b', is_lazy=False)
         assert binding.source_statement == 'from ..a import b as c'
-        assert str(binding) == '..a.b as c'
+        assert binding.imported_name == '..a.b as c'
 
     def test_importfrom_member(self):
         binding = ImportationFrom('b', None, 'a', 'b', is_lazy=False)
         assert binding.source_statement == 'from a import b'
-        assert str(binding) == 'a.b'
+        assert binding.imported_name == 'a.b'
 
     def test_importfrom_submodule_member(self):
         binding = ImportationFrom('c', None, 'a.b', 'c', is_lazy=False)
         assert binding.source_statement == 'from a.b import c'
-        assert str(binding) == 'a.b.c'
+        assert binding.imported_name == 'a.b.c'
 
     def test_importfrom_member_as(self):
         binding = ImportationFrom('c', None, 'a', 'b', is_lazy=False)
         assert binding.source_statement == 'from a import b as c'
-        assert str(binding) == 'a.b as c'
+        assert binding.imported_name == 'a.b as c'
 
     def test_importfrom_submodule_member_as(self):
         binding = ImportationFrom('d', None, 'a.b', 'c', is_lazy=False)
         assert binding.source_statement == 'from a.b import c as d'
-        assert str(binding) == 'a.b.c as d'
+        assert binding.imported_name == 'a.b.c as d'
 
     def test_importfrom_star(self):
         binding = StarImportation('a.b', None)
         assert binding.source_statement == 'from a.b import *'
-        assert str(binding) == 'a.b.*'
+        assert binding.imported_name == 'a.b.*'
 
     def test_importfrom_star_relative(self):
         binding = StarImportation('.b', None)
         assert binding.source_statement == 'from .b import *'
-        assert str(binding) == '.b.*'
+        assert binding.imported_name == '.b.*'
 
     def test_importfrom_future(self):
         binding = FutureImportation('print_function', None, None)
         assert binding.source_statement == 'from __future__ import print_function'
-        assert str(binding) == '__future__.print_function'
+        assert binding.imported_name == '__future__.print_function'
 
     def test_lazy_import(self):
         binding = Importation('a', None, 'a', is_lazy=True)
